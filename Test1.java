32db4d7fbb1e061ec7e63d5ab53c1a6867aa1763
sosichh <mikhailgoncharskiy@yahoo.com>
2025-09-01 01:52:20 +0300

    Test1

---
+++ Test1.java
@@ -0,0 +1,6 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Это первая версия");
+        System.out.println("Обновление класса Test1");
+    }
+}
